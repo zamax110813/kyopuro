72307cf64be06588751721431cf2ca67a7f9b7d9
zamax110813 <47414483+zamax110813@users.noreply.github.com>
2021-11-18 11:34:09 +0900

    a

--- kyopuro/6_8.cpp
+++ kyopuro/6_8.cpp
@@ -14,7 +14,20 @@ int main(void) {
 	long long left = 0;
 	long long right = INF;
 	while (right - left > 1) {
-
+		long long mid = (right+left) / 2;
+		bool ok = true;
+		vector<long long> t(N);
+		for (int i = 0; i < N; i++) {
+			if (H[i] > mid)ok = false;
+			else t[i] = (mid - H[i]) / S[i];
+		}
+		sort(t.begin(), t.end());
+		for (int i = 0; i < N; i++) {
+			if (t[i] < i)ok = false;
+		}
+		if (ok)right = mid;
+		else left = mid;
 	}
+	cout << right << endl;
 
 }
