99dd70e0a36f07afdb5f9921b141275f697c39f4
zamax110813 <47414483+zamax110813@users.noreply.github.com>
2021-11-18 11:10:10 +0900

    a

---
+++ kyopuro/6_8.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const long long INF = 1LL << 60;
+
+int main(void) {
+	int N;
+	cin >> N;
+	vector<long long> H(N),S(N);
+	for (int i = 0; i < N; i++) {
+		cin >> H[i] >> S[i];
+	}
+
+	long long left = 0;
+	long long right = INF;
+	while (right - left > 1) {
+
+	}
+
+}
