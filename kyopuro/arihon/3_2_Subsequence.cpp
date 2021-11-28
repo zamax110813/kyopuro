bf4220208853f3f91238e522f411dce7213fcbe9
zamax110813 <zamax110813@gmail.com>
2021-11-28 18:50:44 +0900

    あ

--- kyopuro/arihon/3_2_Subsequence.cpp
+++ kyopuro/arihon/3_2_Subsequence.cpp
@@ -1,29 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//尺取り法
+
 int main(){
-    int N,S;
+    int N;
+    long long S;
     cin>>N>>S;
-    vector<int> a(N);
-    for(int i=0;i<N;i++){
-        cin>>a[i];
-    }
-    vector<int> sum(N);
+    vector<long long> a(N);
+    for(int i=0;i<N;i++)cin>>a[i];
 
-   for(int i=0;i<N;i++){
-       if(i==0)sum[i]=a[i];
-       else sum[i]=sum[i-1]+a[i];
+    //条件を満たす区間の長さの最小値res
+    int res=N+1;//上界を入れておく
+
+   //区間の左端leftで場合分け
+   int right=0;
+   long long sum=0;
+   for(int left=0;left<N;left++){
+       //[left,right)の総和がS以上となる最小のrightを求める
+       while(right<N&&sum<S){
+           sum+=a[right];
+           right++;
+       }
+
+       //更新
+       if(sum<S)break;//これ以上leftを進めてもだめ
+       res=min(res,right-left);
+
+       if(right==left)right++;
+       else sum-=a[left];
    }
-
-   if(sum[N-1]<S){
-       cout<<"0"<<endl;
-       return 0;
-   }
-
-   int res=N;
-   for(int s=0;sum[s]+S<=sum[N];s++){
-       int t=lower_bound(sum+s,sum+N,sum[s]+S)-sum;
-   }
-
-
+   if(res<N+1)cout<<res<<endl;
+   else cout<<0<<endl;
 }
