3b60e5b3c0f4b00f63636c5e3ac0c36c92203a18
zamax110813 <47414483+zamax110813@users.noreply.github.com>
2021-11-29 22:23:49 +0900

    a

--- atcoder/ABC/abc229/a/main.cpp
+++ atcoder/ABC/abc229/a/main.cpp
@@ -1,10 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define long long ll
 
-int main(){
-    string s1,s2;
-    cin>>s1>>s2;
-    if(s1=="#."&&s2==".#")cout<<"No"<<endl;
-    else if(s1==".#"&&s2=="#.")cout<<"No"<<endl;
-    else cout<<"Yes"<<endl;
+struct UnionFind{
+    vector<int> par,siz;
+    UnionFind(int n):par(n,-1),siz(n,1){}
+    int root(int x){
+        if(par[x]==-1)return x;
+        else return par[x]=root(par[x]);
+    }
+    bool issame(int x,int y){
+        return root(x)==root(y);
+    }
+    bool unite(int x,int y){
+        x=root(x),y=root(y);
+        if(x==y)return false;
+        if(siz[x]<siz[y])swap(x,y);
+        par[y]=x;
+        siz[x]+=siz[y];
+        return true;
+    }
+    int size(int x){
+        return siz[root(x)];
+    }
+};
+
+int main(void){
+    
 }
