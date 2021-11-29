8f8c4fbf324548b5395564c8d7048377451f4397
zamax110813 <47414483+zamax110813@users.noreply.github.com>
2021-11-30 00:24:56 +0900

    a

--- atcoder/ABC/abc229/e/main.cpp
+++ atcoder/ABC/abc229/e/main.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define long long ll
+typedef long long ll;
 
 struct UnionFind{
     vector<int> par,siz;
@@ -26,5 +26,26 @@ struct UnionFind{
 };
 
 int main(void){
-    
+    ll n,m;cin>>n>>m;
+    vector<vector<ll>>v(n+1);
+    while(m--){
+        ll a,b;cin>>a>>b;
+        v[a].push_back(b);
+    }
+    UnionFind d(n+1);
+    vector<ll>res={0}; //頂点 N まで消した時の答えは必ず 0
+    ll ans = 0; // 今の連結成分の数
+    for(ll i=n;i>=2;i--){
+        ans++; //頂点 i を追加
+        for(auto j:v[i]){
+            if(!d.issame(i,j)){
+                d.unite(i,j);
+                ans--; //非連結の頂点同士を繋げたとき、連結成分の数は 1 減る
+            }
+        }
+        res.push_back(ans);
+    }
+    reverse(res.begin(),res.end());
+    for(auto x:res)cout<<x<<endl;
+    return 0;
 }
