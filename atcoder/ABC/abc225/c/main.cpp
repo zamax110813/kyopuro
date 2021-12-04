9b98fd2ff356b0ee9970c829185b2d97d6a08201
zamax110813 <zamax110813@gmail.com>
2021-12-04 18:48:01 +0900

    a

--- atcoder/ABC/abc225/c/main.cpp
+++ atcoder/ABC/abc225/c/main.cpp
@@ -5,15 +5,34 @@ typedef long long ll;
 
 
 int main(void){
-    long long n;
+    int n;
     int m;
     cin>>n>>m;
-    vector<vector<long long>> b(n,vector<long long>(m));
+    vector<vector<int>> b(n,vector<int>(m));
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             cin>>b[i][j];
         }
     }
+    vector<vector<int>> x(n,vector<int>(m)),y(n,vector<int>(m));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            x[i][j]=(b[i][j]+6)/7;
+            y[i][j]=(b[i][j]-1)%7+1;
+        }
+    }
+    bool ok=true;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(i>0&&x[i][j]!=x[i-1][j]+1)ok=false;
+            if(j>0&&x[i][j]!=x[i][j-1])ok=false;
+            if(j>0&&y[i][j]!=y[i][j-1]+1)ok=false;
+            if(i>0&&y[i][j]!=y[i-1][j])ok=false;
+        }
+    }
+
+    if(ok)cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
 
 }
 
