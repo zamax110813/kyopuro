5aced59732d7d79baee3cb1fdd3becef82a7c654
zamax110813 <zamax110813@gmail.com>
2021-12-09 18:06:13 +0900

    a

--- kyopuro/arihon/2/2_3/lcs.cpp
+++ kyopuro/arihon/2/2_3/lcs.cpp
@@ -7,4 +7,12 @@ int main(void){
     cin>>n>>m;
     string s,t;
     cin>>s>>t;
+    vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(s[i]==t[j])dp[i+1][j+1]=dp[i][j]+1;
+            else dp[i+1][j+1]=max(dp[i][j+1],dp[i+1][j]);
+        }
+    }
+    cout<<dp[n][m]<<endl;
 }
