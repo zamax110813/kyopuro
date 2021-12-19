3f1769e0cef7524f841046872bda41532714e392
zamax110813 <zamax110813@gmail.com>
2021-12-19 18:05:11 +0900

    a

--- kyopuro/atcoder/ABC/abc220/c/main.cpp
+++ kyopuro/atcoder/ABC/abc220/c/main.cpp
@@ -5,15 +5,31 @@ typedef long long ll;
 
 
 int main(void){
-    int N;
+    ll N;
     cin>>N;
-    vector<int> A(N);
+    vector<ll> A(N);
     for(int i=0;i<N;i++){
         cin>>A[i];
     }
     ll X;
     cin>>X;
-    
+    ll sumA=0;
+    for(int i=0;i<N;i++){
+        sumA+=A[i];
+    }
+    ll ans=0;
+    ans+=N*(X/sumA);
+    sumA=(X/sumA)*sumA;
+    for(int i=0;i<N;i++){
+        sumA+=A[i];
+        ans++;
+        if(sumA>X){
+            cout<<ans<<endl;
+            return 0;
+        }
+    }
+    return 0;
+
 }
 
 
--- kyopuro/atcoder/ABC/abc220/d/main.cpp
+++ kyopuro/atcoder/ABC/abc220/d/main.cpp
@@ -1,11 +1,32 @@
 #include<bits/stdc++.h>
+#include<atcoder/all>
 using namespace std;
+using namespace atcoder;
 typedef long long ll;
-
+using mint=modint998244353;
 
 
 int main(void){
+    ll N;
+    cin>>N;
+    vector<ll> A(N+1);
+    for(int i=1;i<=N;i++){
+        cin>>A[i];
+    }
+    mint dp[N+1][10];
+    memset(dp,0,sizeof(dp));
+    dp[1][A[1]]=1;
 
+    for(ll i=1;i<=N-1;i++){
+        for(ll j=0;j<10;j++){
+            dp[i+1][(j+A[i+1])%10]+=dp[i][j];
+            dp[i+1][(j*A[i+1])%10]+=dp[i][j];
+        }
+    }
+    for(int i=0;i<10;i++){
+        cout<<dp[N][i].val()<<endl;
+    }
+    return 0;
 }
 
 
