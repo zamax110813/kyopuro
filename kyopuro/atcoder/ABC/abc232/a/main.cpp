f97e4e112af2ec8720818e34aa104c03b48b4362
zamax110813 <zamax110813@gmail.com>
2021-12-21 13:26:22 +0900

    あ

---
+++ kyopuro/atcoder/ABC/abc232/a/main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+
+int main(void){
+    string s;
+    cin>>s;
+    cout<<(s[0]-'0')*(s[2]-'0')<<endl;
+}
+
+
---
+++ kyopuro/atcoder/ABC/abc232/b/main.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+
+int main(void){
+    string s,t;
+    cin>>s>>t;
+    for(int i=0;i<26;i++){
+        for(int j=0;j<n;j++){
+            s[j]=s[i]
+        }
+    }
+}
+
+
