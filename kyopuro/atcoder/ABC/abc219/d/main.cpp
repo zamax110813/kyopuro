6d1397d386db8c83b6286e8e6bbcc69241c88c23
zamax110813 <47414483+zamax110813@users.noreply.github.com>
2021-12-23 01:43:00 +0900

    a

--- kyopuro/atcoder/ABC/abc219/d/main.cpp
+++ kyopuro/atcoder/ABC/abc219/d/main.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-
+const long long INF=1LL<<60;
 
 int main(void){
     int n;
@@ -10,6 +10,31 @@ int main(void){
     cin>>x>>y;
     vector<int> a(n),b(n);
     for(int i=0;i<n;i++){
-        cin>>a[i]
+        cin>>a[i]>>b[i];
+    }
+    ll dp[310][310][310];
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<301;j++){
+            for(int k=0;k<301;k++){
+                dp[i][j][k]=INF;
+            }
+        }
+    }
+    dp[0][0][0]=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<x+1;j++){
+            for(int k=0;k<y+1;k++){
+                if(dp[i][j][k]<INF){
+                    dp[i+1][min(j+a[i],x)][min(k+b[i],y)]=min(dp[i+1][min(j+a[i],x)][min(k+b[i],y)],dp[i][j][k]+1);
+                    dp[i+1][j][k]=min(dp[i+1][j][k],dp[i][j][k]);
+                }
+            }
+        }
+    }
+    if(dp[n][x][y]==INF){
+        cout<<-1<<endl;
+        return 0;
+    }else{
+        cout<<dp[n][x][y]<<endl;
     }
 }
