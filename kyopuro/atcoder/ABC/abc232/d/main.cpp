d0c794172d7f585092527c38136f2916958e9c52
zamax110813 <47414483+zamax110813@users.noreply.github.com>
2021-12-22 14:15:37 +0900

    a

--- kyopuro/atcoder/ABC/abc232/d/main.cpp
+++ kyopuro/atcoder/ABC/abc232/d/main.cpp
@@ -13,6 +13,14 @@ int main(void){
             cin>>c[i][j];
         }
     }
+    vector<vector<int>> f(h+1,vector<int>(w+1));
+    for(int i=h-1;i>=0;i--){
+        for(int j=w-1;j>=0;j--){
+            if(c[i][j]=='#')continue;
+            f[i][j]=max(f[i+1][j],f[i][j+1])+1;
+        }
+    }
+    cout<<f[0][0]<<endl;
 
 }
 
