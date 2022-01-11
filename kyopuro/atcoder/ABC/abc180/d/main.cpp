b509a13e7d574a9626e7d7dd8e49b0be6655abf3
zamax110813 <zamax110813@gmail.com>
2022-01-11 19:44:31 +0900

    a

---
+++ kyopuro/atcoder/ABC/abc180/d/main.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+typedef long long ll;
+#define REP(i,n) for(ll i=0;i<(ll)n;i++)
+#define REPD(i,n) for(ll i=n-1;i>=0;i--)
+#define FOR(i,a,b) for(ll i=a;i<=(ll)b;i++)
+#define FORD(i,a,b) for(ll i=a;i>=(ll)b;i--)
+#define FORA(i,I) for(const auto& i:I)
+#define ALL(x) x.begin(),x.end()
+#define SIZE(x) ll(x.size())
+#define F first
+#define S second
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; } 
+
+int main(void){
+        ll x,y;
+        ll a,b;
+        cin>>x>>y>>a>>b;
+        ll exp=0;
+        while(a*x<=x+b&&x<(double)y/a){
+                exp++;
+                x*=a;
+        }
+        exp+=(y-1-x)/b;
+        cout<<exp<<endl;
+}
+
