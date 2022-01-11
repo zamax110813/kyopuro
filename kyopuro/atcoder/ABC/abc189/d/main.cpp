aac2c0a716bf0115b92148a3cb166b6ca9284647
zamax110813 <47414483+zamax110813@users.noreply.github.com>
2022-01-11 11:23:29 +0900

    j

--- kyopuro/atcoder/ABC/abc189/d/main.cpp
+++ kyopuro/atcoder/ABC/abc189/d/main.cpp
@@ -17,7 +17,20 @@ int main(void){
     int n;
     cin>>n;
     vector<string> s(n);
-    
+    REP(i,n)cin>>s[i];
+    ll dp[65][2];
+    dp[0][0]=1;
+    dp[0][1]=1;
+    REP(i,n){
+        if(s[i]=="AND"){
+            dp[i+1][1]=dp[i][1];
+            dp[i+1][0]=dp[i][1]+2*dp[i][0];
+        }else{
+            dp[i+1][1]=dp[i][0]+2*dp[i][1];
+            dp[i+1][0]=dp[i][0];
+        }
+    }
+    cout<<dp[n][1]<<endl;
 }
 
 
