c4387e14493503ecf17e289357fe0082626c6270
zamax110813 <zamax110813@gmail.com>
2022-01-17 20:07:24 +0900

    a

---
+++ kyopuro/atcoder/ABC/abc144/d/main.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+typedef long long ll;
+#define REP(i,n) for(ll i=0;i<(ll)n;i++)
+#define REPD(i,n) for(ll i=n-1;i>=0;i--)
+#define FOR(i,a,b) for(ll i=a;i<=(ll)b;i++)
+#define FORD(i,a,b) for(ll i=a;i>=(ll)b;i--)
+#define FORA(i,I) for(const auto& i:I)
+#define ALL(x) x.begin(),x.end()
+#define SIZE(x) ll(x.size())
+#define F first
+#define S second
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; } 
+
+int main(void){
+        const double PI=acos(-1.0);
+   double a,b,x;
+   cin>>a>>b>>x;
+   x/=a;
+   cout<<fixed<<setprecision(10);
+   if(x>a*b/2)cout<<atan2(2*(a*b-x),a*a)*180/PI<<endl;
+   else cout<<atan2(b*b,2*x)*180/PI<<endl;
+}
+
