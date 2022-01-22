d3ceb3e711e410da9d1619e4cefaa24407be4c0a
zamax110813 <zamax110813@gmail.com>
2022-01-22 17:58:44 +0900

    a

--- kyopuro/atcoder/ABC/abc058/c/main.cpp
+++ kyopuro/atcoder/ABC/abc058/c/main.cpp
@@ -16,19 +16,29 @@ typedef long long ll;
 template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
 template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; } 
 
+
+int num[50][26];
+
 int main(void){
    int n;
    cin>>n;
-	int dp[1000]
-   vector<string> s(n);
-   map<char,int> map();
    REP(i,n){
-           	cin>>s[i];
-			sort(s[i].begin(),s[i].end)());
-			for(auto v:s[i]){
-				map[v]++;
+           	string s;
+			cin>>s;
+			for(char c:s){
+				num[i][c-'a']++;
 			}
    }
+   string ans="";
+   REP(i,26){
+	   int mi=5000;
+	   REP(j,n){
+		   mi=min(mi,num[j][i]);
+	   }
+	   char c='a'+i;
+	   REP(i,mi)ans+=c;
+   }
+   cout << ans << endl;
 
 
 }
