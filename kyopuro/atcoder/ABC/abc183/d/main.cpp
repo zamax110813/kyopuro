586c64a12cc7a83c881632bd93620306d6a1a528
zamax110813 <zamax110813@gmail.com>
2022-01-22 20:21:03 +0900

    tukareta

--- kyopuro/atcoder/ABC/abc183/d/main.cpp
+++ kyopuro/atcoder/ABC/abc183/d/main.cpp
@@ -16,7 +16,25 @@ typedef long long ll;
 template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
 template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; } 
 
+const int MAX=210000;
+
 int main(void){
-    
+    ll n,w;
+    cin>>n>>w;
+    auto solve=[&](){
+            vector<ll> num(MAX+1,0);
+            REP(i,n){
+                    ll s,t,p;
+                    cin>>s>>t>>p;
+                    num[s]+=p,num[t]-=p;
+            }
+            REP(j,MAX){
+                    num[j+1]+=num[j];
+                    if(num[j]>w)return false;
+            }
+            return true;
+    };
+    cout<<(solve()?"Yes":"No")<<endl;
+
 }
 
