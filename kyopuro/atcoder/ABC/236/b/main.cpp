9ee0b79cf061c8c8042717cb97a47d9c8b5d613a
zamax110813 <47414483+zamax110813@users.noreply.github.com>
2022-01-24 20:51:07 +0900

    あ

---
+++ kyopuro/atcoder/ABC/236/b/main.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+typedef long long ll;
+#define REP(i,n) for(ll i=0;i<(ll)n;i++)
+#define REPD(i,n) for(ll i=n-1;i>=0;i--)
+#define FOR(i,a,b) for(ll i=a;i<=(ll)b;i++)
+#define FORD(i,a,b) for(ll i=a;i>=(ll)b;i--)
+#define FORA(i,I) for(const auto& i:I)
+#define ALL(x) x.begin(),x.end()
+#define SIZE(x) ll(x.size())
+#define F first
+#define S second
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; } 
+
+int main(void){
+    int n;
+    cin>>n;
+    vector<int> a(4*n-1);
+    vector<int> v(n,0);
+    REP(i,4*n-1){
+        cin>>a[i];
+        v[a[i]-1]++;
+    }
+    REP(i,n){
+        if(v[i]==3){
+            cout<<i+1<<endl;
+            return 0;
+        }
+    }
+
+
+}
---
+++ kyopuro/atcoder/ABC/236/d/main.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+typedef long long ll;
+#define REP(i,n) for(ll i=0;i<(ll)n;i++)
+#define REPD(i,n) for(ll i=n-1;i>=0;i--)
+#define FOR(i,a,b) for(ll i=a;i<=(ll)b;i++)
+#define FORD(i,a,b) for(ll i=a;i>=(ll)b;i--)
+#define FORA(i,I) for(const auto& i:I)
+#define ALL(x) x.begin(),x.end()
+#define SIZE(x) ll(x.size())
+#define F first
+#define S second
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; } 
+
+int main(void){
+    int n,m;
+    cin>>n>>m;
+    vector<string> s(n),t(m);
+    map<string,int> map;
+    REP(i,n){
+        cin>>s[i];
+        map[s[i]]++;
+    }
+    REP(i,m){
+        cin>>t[i];
+        map[t[i]]++;
+    }
+    REP(i,n){
+        if(map[s[i]]==1)cout<<"No"<<endl;
+        else cout<<"Yes"<<endl;
+    }
+
+}
