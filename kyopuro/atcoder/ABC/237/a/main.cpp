ef6ef9be649c8a3f200ed2c9afda627007bcf092
zamax110813 <47414483+zamax110813@users.noreply.github.com>
2022-02-05 20:46:30 +0900

    a

---
+++ kyopuro/atcoder/ABC/237/a/main.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+typedef long long ll;
+#define REP(i,n) for(ll i=0;i<(ll)n;i++)
+#define REPD(i,n) for(ll i=n-1;i>=0;i--)
+#define FOR(i,a,b) for(ll i=a;i<=(ll)b;i++)
+#define FORD(i,a,b) for(ll i=a;i>=(ll)b;i--)
+#define FORA(i,I) for(const auto& i:I)
+#define ALL(x) x.begin(),x.end()
+#define SIZE(x) ll(x.size())
+#define F first
+#define S second
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; } 
+
+int main(void){
+    ll n;
+    cin>>n;
+    if(n>=ll(-pow(2,31))&&n<ll(pow(2,31))){
+        cout<<"Yes"<<endl;
+    }else{
+        cout<<"No"<<endl;
+    }
+    
+}
