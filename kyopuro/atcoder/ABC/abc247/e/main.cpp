3d367db0b85c0dbf3265e2dd59cfbf69497dad79
zamax110813 <47414483+zamax110813@users.noreply.github.com>
2022-04-28 02:30:16 +0900

    a

--- kyopuro/atcoder/ABC/abc247/e/main.cpp
+++ kyopuro/atcoder/ABC/abc247/e/main.cpp
@@ -16,6 +16,43 @@ typedef long long ll;
 template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
 template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; } 
 
-int main(void){
+const int MAX=200000;
 
+int n,x,y;
+int a[MAX];
+
+ll solve(const vector<ll>& b){
+    ll ans=0;
+    int n=b.size();
+    int right=0;
+    int cntx=0,cnty=0;
+    for(int left=0;left<n;left++){
+        while(right<n&&!(cntx>=1&&cnty>=1)){
+            if(b[right]==x)cntx++;
+            if(b[right]==y)cnty++;
+            right++;
+        }
+        if(cntx>=1&&cnty>=1)ans+=n-right+1;
+        if(b[left]==x)cntx--;
+        if(b[left]==y)cnty--;
+    }
+    return ans;
+}
+
+int main(void){
+    cin>>n>>x>>y;
+    vector<ll> a(n);
+    REP(i,n)cin>>a[i];
+    vector<ll> b;
+    ll ans=0;
+    for(int i=0;i<n;i++){
+        if(a[i]>x||a[i]<y){
+            ans+=solve(b);
+            b.clear();
+        }else{
+            b.push_back(a[i]);
+        }
+    }
+    ans+=solve(b);
+    cout<<ans<<endl;
 }
