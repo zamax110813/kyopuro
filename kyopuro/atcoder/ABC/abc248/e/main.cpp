a7d1796b3b869f2e111fdf3aba197d45cb4db00d
zamax110813 <zamax110813@gmail.com>
2022-04-27 17:45:59 +0900

    a

--- kyopuro/atcoder/ABC/abc248/e/main.cpp
+++ kyopuro/atcoder/ABC/abc248/e/main.cpp
@@ -18,15 +18,44 @@ template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } retu
 
 const long long INF=1LL<<60;
 
+long long x[300],y[300];
+bool flag[300][300];
+
+bool colinear(int i,int j,int k){
+    long long val1=(y[k]-y[i])*(x[j]-x[i]);
+    long long val2=(y[j]-y[i])*(x[k]-x[i]);
+    return (val1==val2);
+}
+
 int main(void){
-    int n,k;
-    cin>>n>>k;
-    if(k==1){
+    int n,K;
+    cin>>n>>K;
+    if(K==1){
         cout<<"Infinity"<<endl;
         return 0;
     }
-    vector<pair<int,int>> idx(n);
-    REP(i,n)cin>>idx[i].first>>idx[i].second;
-    
+    REP(i,n)cin>>x[i]>>y[i];
+    for(int i=0;i<n;i++)for(int j=i+1;j<n;j++)flag[i][j]=true;
+    int ans=0;
+    vector<int> used;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(!flag[i][j])continue;
+            used.clear();
+            used.push_back(i);
+            used.push_back(j);
+            int cnt=2;
+            for(int k=j+1;k<n;k++){
+                if(colinear(i,j,k)){
+                    cnt++;
+                    used.push_back(k);
+                }
+            }
+            if(cnt>=K)ans++;
+            for(int ii=0;ii<cnt;ii++)for(int jj=ii+1;jj<cnt;jj++)flag[used[ii]][used[jj]]=false;
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
 }
 
