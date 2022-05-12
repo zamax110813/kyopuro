4af780a32df411f1a954d52ac41179735360916a
zamax110813 <47414483+zamax110813@users.noreply.github.com>
2022-05-12 16:02:35 +0900

    あ

---
+++ kyopuro/atcoder/ABC/abc102/c/main.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+typedef long long ll;
+#define REP(i,n) for(ll i=0;i<(ll)n;i++)
+#define REPD(i,n) for(ll i=n-1;i>=0;i--)
+#define FOR(i,a,b) for(ll i=a;i<=(ll)b;i++)
+#define FORD(i,a,b) for(ll i=a;i>=(ll)b;i--)
+#define FORA(i,I) for(const auto& i:I)
+#define ALL(x) x.begin(),x.end()
+#define SIZE(x) ll(x.size())
+#define F first
+#define S second
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; } 
+
+const long long INF=1LL<<60;
+
+int main(void){
+    ll n;
+    cin>>n;
+    vector<ll> A(n),B(n);
+    REP(i,n)cin>>A[i],B[i]=A[i]-i;
+    sort(ALL(B));
+    int b=B[n/2];
+    ll ans=0;
+    REP(i,n)ans+=abs(b-B[i]);
+    cout<<ans<<endl;
+}
---
+++ kyopuro/atcoder/caddi2018b/d/main.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+typedef long long ll;
+#define REP(i,n) for(ll i=0;i<(ll)n;i++)
+#define REPD(i,n) for(ll i=n-1;i>=0;i--)
+#define FOR(i,a,b) for(ll i=a;i<=(ll)b;i++)
+#define FORD(i,a,b) for(ll i=a;i>=(ll)b;i--)
+#define FORA(i,I) for(const auto& i:I)
+#define ALL(x) x.begin(),x.end()
+#define SIZE(x) ll(x.size())
+#define F first
+#define S second
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; } 
+
+const long long INF=1LL<<60;
+
+int main(void){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++)cin>>a[i];
+    bool exist_odd=false;
+    REP(i,n)if(a[i]%2==1)exist_odd=true;
+    if(exist_odd)cout<<"first"<<endl;
+    else cout<<"second"<<endl;
+}
