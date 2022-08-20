33fb794d0d73adb1dc71263e3c2c65d3c60d8830
zamax110813 <47414483+zamax110813@users.noreply.github.com>
2022-08-20 20:30:05 +0900

    a

--- kyopuro/atcoder/ABC/abc264/e/main.cpp
+++ kyopuro/atcoder/ABC/abc264/e/main.cpp
@@ -18,6 +18,43 @@ template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } retu
 
 const long long INF=1LL<<60;
 
+struct UnionFind{
+    vector<int> par,siz;
+    UnionFind(int n):par(n,-1),siz(n,1){}
+    int root(int x){
+        if(par[x]==-1)return x;
+        else return par[x]=root(par[x]);
+    }
+    bool issame(int x,int y){
+        return root(x)==root(y);
+    }
+    bool unite(int x,int y){
+        x=root(x),y=root(y);
+        if(x==y)return false;
+        if(siz[x]<siz[y])swap(x,y);
+        par[y]=x;
+        siz[x]+=siz[y];
+        return true;
+    }
+    int size(int x){
+        return siz[root(x)];
+    }
+};
+
 int main(void){
+    int n,m,e;
+    cin>>n>>m>>e;
+    vector<int> u(e),v(e);
+    for(int i=0;i<e;i++){
+        cin>>u[i]>>v[i];
+        u[i]--;v[i]--;
+    }
+    int q;
+    cin>>q;
+    vector<int> query(q);
+    for(int i=0;i<q;i++){
+
+    }
     
+
 }
